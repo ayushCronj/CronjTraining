7bf5139be47c25f4a46ea00a035caf7a1e247c9b
ayushcronj <ayush@cronj.com>
2019-06-20 18:04:37 +0530

    HTML_CSS

---
+++ HTML_CSS_Assignment/script.js
@@ -0,0 +1,44 @@
+document.getElementById("result1").style.display = "none";
+document.getElementById("result2").style.display = "none";
+document.getElementById("result3").style.display = "none";
+document.getElementById("result4").style.display = "none";
+function validate1()
+{
+	var email1 = "ayush@cronj.com";
+	var pass1 = "qwerty";
+	if( document.getElementById('email').value == "")
+	{
+			document.getElementById("result3").style.display = "block";
+			document.getElementById("result1").style.display = "none";
+			document.getElementById("result2").style.display = "none";
+			document.getElementById("result4").style.display = "none";
+	}
+	else if(document.getElementById('pass').value == "")
+	{
+			document.getElementById("result4").style.display = "block";
+			document.getElementById("result1").style.display = "none";
+			document.getElementById("result2").style.display = "none";
+			document.getElementById("result3").style.display = "none";
+	}
+	else if( document.getElementById('email').value == email1 &&  document.getElementById('pass').value == pass1)
+		{
+			document.getElementById("result1").style.display = "block";
+			document.getElementById("result2").style.display = "none";
+			document.getElementById("result3").style.display = "none";
+			document.getElementById("result4").style.display = "none";
+		}
+	else
+		{
+			document.getElementById("result2").style.display = "block";
+			document.getElementById("result1").style.display = "none";
+			document.getElementById("result3").style.display = "none";
+			document.getElementById("result4").style.display = "none";
+		}
+}
+function validate2()
+{
+document.getElementById("result1").style.display = "none";
+document.getElementById("result2").style.display = "none";
+document.getElementById("result3").style.display = "none";
+document.getElementById("result4").style.display = "none";
+}
